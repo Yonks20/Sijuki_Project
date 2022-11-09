993806586ecfcc1b2cbfcfdaea43e1bb26b7813a
yonks20 <yonkieyudha@gmail.com>
2022-11-09 11:13:59 +0700

    landing page fix banget

--- pages/landing.php
+++ pages/landing.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link rel="stylesheet" href="style.css" class="href">
   </head>
   <body>
@@ -31,13 +32,20 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <h1>Selamat datang di For-Pi</h1>
-        <a href="../login.php" class="btn btn-primary" > Login </a>
+        <h1 class= "text-white" data-aos="fade-up"
+     data-aos-anchor-placement="center-bottom" >Selamat datang di For-Pi</h1>
+        <br>
+        <a href="../login.php" class="btn btn-primary" data-aos="fade-up"
+     data-aos-anchor-placement="center-bottom" > Login </a>
       </div>
     </div>
   </div>
 
 </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
   </body>
 </html>
